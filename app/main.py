9012b6ab17d137c6915662975f0ff76fc558a08a
codebyaadi <adityastar57@gmail.com>
2025-01-01 14:06:11 +0000

    feat: added lifespan for creating session and syncing tables on start up

--- app/main.py
+++ app/main.py
@@ -1,8 +1,34 @@
-from typing import Union
-
+import logging
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
+from typing import Union
+from cassandra.cqlengine.management import sync_table
 
-app = FastAPI()
+from app.models import Product, ProductScrapeEvent
+from app.db import get_session
+
+
+# Set up logging for better debugging and error messages
+logging.basicConfig(level=logging.INFO)
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    session = get_session()
+    app.state.session = session
+    try:
+        logging.info("Starting table synchronization...")
+        sync_table(Product)
+        sync_table(ProductScrapeEvent)
+        logging.info("Table synchronization completed successfully.")
+        yield
+    finally:
+        session.shutdown()
+        app.state.session = None
+        logging.info("Session connections closed gracefully.")
+
+
+app = FastAPI(lifespan=lifespan)
 
 
 @app.get("/")
