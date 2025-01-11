b58b7f35dbcd2e644498533089b1f9f99202b559
codebyaadi <adityastar57@gmail.com>
2025-01-11 14:41:36 +0000

    added new schema and route for Product Event Detail

--- app/main.py
+++ app/main.py
@@ -7,7 +7,7 @@ from cassandra.cqlengine.management import sync_table
 from app.core.config import get_settings
 from app.db import get_session
 from app.models import Product, ProductScrapeEvent
-from app.schema import ProductSchema
+from app.schema import ProductSchema, ProductScrapeEventDetailSchema
 
 
 # Set up logging for better debugging and error messages
@@ -49,3 +49,12 @@ def read_root():
 @app.get("/products", response_model=List[ProductSchema])
 def get_all_products():
     return list(Product.objects.all())
+
+
+@app.get("/products/{asin}")
+def find_product_by_asin(asin: str):
+    data = dict(Product.objects.get(asin=asin))
+    events = list(ProductScrapeEvent.objects().filter(asin=asin))
+    events = [ProductScrapeEventDetailSchema(**x) for x in events]
+    data["events"] = events
+    return data
--- app/schema.py
+++ app/schema.py
@@ -1,6 +1,9 @@
 from uuid import UUID
-from pydantic import BaseModel
+from datetime import datetime
+from pydantic import BaseModel, model_validator
 from typing import Optional
+
+from app.utils.time import uuid1_time_to_datetime
 
 
 class ProductSchema(BaseModel):
@@ -29,3 +32,29 @@ class ProductScrapeEventSchema(BaseModel):
     uuid: UUID
     asin: str
     title: Optional[str]
+
+
+class ProductScrapeEventDetailSchema(BaseModel):
+    """
+    Schema for product scrape event details.
+
+    Attributes:
+        asin (str): The Amazon Standard Identification Number of the product.
+        title (Optional[str]): The title of the product.
+        price_str (Optional[str]): The price of the product as a string.
+        created_at (Optional[datetime]): The timestamp when the event was created, extracted from the UUID.
+
+    Methods:
+        extract_time_from_uuid(cls, values):
+            Extracts the creation time from the UUID and sets it to the created_at attribute.
+    """
+
+    asin: str
+    title: Optional[str]
+    price_str: Optional[str]
+    created_at: Optional[datetime] = None
+
+    @model_validator(mode="before")
+    def extract_time_from_uuid(cls, values):
+        values["created_at"] = uuid1_time_to_datetime(values["uuid"].time)
+        return values
